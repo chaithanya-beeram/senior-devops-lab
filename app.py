c9154bf2319b6168330c1b009688ae161ecad320
chaithanya-beeram <b.chaithanya.prasad@gmail.com>
2026-02-10 06:30:15 +0000

    Updated app to reflect my name and current time

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask
 import os
+from datetime import datetime
 app = Flask(__name__)
 @app.route('/')
 def home():
-    return f"<h1>Build #1: Success</h1><p>Running on: {os.getenv('HOSTNAME')}</p>"
+    # Combine everything into one string
+    line1 = f"<h1>Build #1: Success</h1>"
+    line2 = f"<h3>This is Chaybeeram running his first devops application</h3>"
+    line3 = f"<p>Running on: {os.getenv('HOSTNAME')}</p>"
+    line4 = f"<p>Application running at: {datetime.now()}</p>"
+    
+    return line1 + line2 + line3 + line4
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
