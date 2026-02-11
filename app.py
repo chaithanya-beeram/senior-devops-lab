852ae1fbb6b4a7496efc1a8c279dc3a4f33d304f
chaithanya-beeram <b.chaithanya.prasad@gmail.com>
2026-02-11 23:56:35 +0000

    Add Redis Layer

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask
+import redis
 import os
-from datetime import datetime
+
 app = Flask(__name__)
-@app.route('/')
-def home():
-    # Combine everything into one string
-    line1 = f"<h1>Build #2: Success</h1>"
-    line2 = f"<h3>This is Chaybeeram running his first devops application with an ingress controller</h3>"
-    line3 = f"<p>Running on: {os.getenv('HOSTNAME')}</p>"
-    line4 = f"<p>Application running at: {datetime.now()}</p>"
-    
-    return line1 + line2 + line3 + line4
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+
+# Connect to Redis using the Kubernetes Service name 'redis-service'
+cache = redis.Redis(host='redis-service', port=6379)
+
+@app.get('/')
+def hello():
+    # Increment the counter in Redis
+    count = cache.incr('hits')
+    return f"<h1>Hello from Kubernetes & Chaybeeram!</h1><p>I have been visited <b>{count}</b> times.</p>"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
